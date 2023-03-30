5bf480513b710c4a5580c5fdc46946183c05c0a7
satyam713 <99085730+satyam713@users.noreply.github.com>
2023-03-30 13:31:41 +0530

    Create settergetter.sol

---
+++ settergetter.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+ 
+pragma solidity >=0.7.0 <0.9.0;
+
+contract GetSet {
+
+    
+    uint num1;
+    uint num2;
+    string letter;
+    address addr;
+
+    function get() public view returns( uint,uint,address, string memory) {
+        return ( num1, num2, addr, letter);
+      
+    }
+
+    function set(uint _num1, uint _num2 ,string memory _letter,address _addr ) public  { 
+        num1 = _num1;
+        num2 = _num2;
+        letter = _letter;
+        addr = _addr;
+    }
+}
